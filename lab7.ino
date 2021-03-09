c21a86f9a9a7539f6da13e7a7454b46ec55fb7cc
PMAHC1999 <68324425+PMAHC1999@users.noreply.github.com>
2021-03-09 12:47:03 +0700

    add new file
    Lab#7 NB-IoT

---
+++ lab7.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+#include "Magellan_BC95.h"
+#define DHTTYPE DHT22
+#define DHTPIN 7 // ต่อเข้าขา 7
+//Token Key you can get from magellan platform
+Magellan_BC95 magel; 
+char auth[] = "bbfd9f42-608a-4de1-adad-ccb7c0ce4124"; // น ารหัสมาใส่บริเวณนี้
+String payload;
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(9600);
+  //init Magellan LIB
+  magel.begin();
+  // init dht
+  dht.begin();
+}
+
+void loop() {
+    delay(2000);
+    // get dht22 data
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    // convert to string
+    String Temperature=String(t);
+    String Humidity=String(h);
+    payload="{\"Temperature\":"+Temperature+",\"Humidity\":"+Humidity+"}";
+    magel.report(payload);
+}
